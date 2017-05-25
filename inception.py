0a9ee7fa8f9ceee8fa2a94eed62e80e1ebc50c56
Bartosz Ufnal <ufnalbartosz@gmail.com>
2017-05-25 18:29:16 +0200

    Add initial model in tflean

---
+++ inception.py
@@ -0,0 +1,48 @@
+from __future__ import division, print_function, absolute_import
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Data loading and preprocessing
+from prepare_dataset import maybe_download_and_extract
+
+dataset = maybe_download_and_extract()
+X = dataset['train_images']
+Y = dataset['train_labels']
+X_test = dataset['test_images']
+Y_test = dataset['test_labels']
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+network = input_data(shape=[None, 32, 32, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+network = conv_2d(network, 32, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 512, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 10, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Train using classifier
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y_test),
+          show_metric=True, batch_size=96, run_id='cifar10_cnn')
